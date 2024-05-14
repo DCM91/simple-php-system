8408bfe8946bd4b217ea5ba9ee299aa8904e38f8
DCM91 <castromartindaniel91@gmail.com>
2024-05-14 16:24:27 +0200

    cambio en config

--- php/config.php
+++ php/config.php
@@ -1,5 +1,5 @@
 <?php 
  
- $con = mysqli_connect("localhost","root","","pt_dcm") or die("Couldn't connect");
+    $con = mysqli_connect("localhost","root","","pt_dcm") or die("Couldn't connect");
 
 ?>
